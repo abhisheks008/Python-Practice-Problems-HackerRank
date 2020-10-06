d58966464645c8eee3cf30e550fddda0e6ab577a
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-10-06 10:57:25 +0530

    Sort Data

---
+++ BuiltIns/sort data.py
@@ -0,0 +1,9 @@
+n, m = map(int,input().split())
+ar = []
+for i in range(n):
+    ar.append(list(map(int,input().split())))
+k = int(input())
+ar = sorted(ar,key = lambda x:x[k])
+for i in ar:
+    [print(x,end=' ') for x in i]
+    print('')
